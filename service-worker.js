6e53a37fc26bde1d6be0185a35eb9b94f537ba95
Joseph Chu <hotcupofjoe2013@gmail.com>
2025-09-22 21:05:17 -0400

    Updates

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+// Simple service worker for offline support
+const CACHE_NAME = "recipe-cost-cache-v1";
+const urlsToCache = ["/", "/index.html", "/vite.svg", "/manifest.webmanifest"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
